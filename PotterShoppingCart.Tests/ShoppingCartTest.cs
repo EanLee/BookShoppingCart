2c6ace2dddba0143858d1debb3085d1c6092424e
Ean Lee <ean@ebmtech.com>
2017-08-29 18:42:32 +0800

    增加第4個需求的測試案例 需求: 如果你買了4本不同的書，則會有20%的折扣

--- PotterShoppingCart.Tests/ShoppingCartTest.cs
+++ PotterShoppingCart.Tests/ShoppingCartTest.cs
@@ -99,5 +99,28 @@ namespace PotterShoppingCart.Tests
             //  assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void CostCalcaulte_如果是四本不同的書_打八折()
+        {
+            //  arrange
+            var books = new List<Book>()
+            {
+                new Book{Name="哈利波特1", ISDN="00001", Amount=1},
+                new Book{Name="哈利波特2", ISDN="00002", Amount=1},
+                new Book{Name="哈利波特3", ISDN="00003", Amount=1},
+                new Book{Name="哈利波特4", ISDN="00004", Amount=0},
+                new Book{Name="哈利波特5", ISDN="00005", Amount=1},
+            };
+
+            int expected = Convert.ToInt32(100 * 3 * 0.9);
+            var cashier = new CartCashier();
+
+            //  ack
+            int actual = cashier.GetExpense(books);
+
+            //  assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }

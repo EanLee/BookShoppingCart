2b9b2134dda96416fdb7f1575269ff56073d66af
Ean Lee <ean@ebmtech.com>
2017-08-29 18:25:52 +0800

    增加第三個需求的測試案例 需求: 如果你買了三本不同的書，則會有10%的折扣

--- PotterShoppingCart.Tests/ShoppingCartTest.cs
+++ PotterShoppingCart.Tests/ShoppingCartTest.cs
@@ -44,7 +44,7 @@ namespace PotterShoppingCart.Tests
                 new Book{Name="哈利波特5", ISDN="00005", Amount=0},
             };
 
-            int expected = Convert.ToInt32(200*0.95);
+            int expected = Convert.ToInt32(200 * 0.95);
             var cashier = new CartCashier();
 
             //  ack
@@ -68,6 +68,29 @@ namespace PotterShoppingCart.Tests
             };
 
             int expected = 200;
+            var cashier = new CartCashier();
+
+            //  ack
+            int actual = cashier.GetExpense(books);
+
+            //  assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CostCalcaulte_買3本不同的書_則會有九折()
+        {
+            //  arrange
+            var books = new List<Book>()
+            {
+                new Book{Name="哈利波特1", ISDN="00001", Amount=1},
+                new Book{Name="哈利波特2", ISDN="00002", Amount=1},
+                new Book{Name="哈利波特3", ISDN="00003", Amount=1},
+                new Book{Name="哈利波特4", ISDN="00004", Amount=0},
+                new Book{Name="哈利波特5", ISDN="00005", Amount=0},
+            };
+
+            int expected = Convert.ToInt32(100 * 3 * 0.9);
             var cashier = new CartCashier();
 
             //  ack

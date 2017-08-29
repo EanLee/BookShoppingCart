a56c5d15a12a296160676ff61aee864309632ddc
Ean Lee <ean@ebmtech.com>
2017-08-29 17:44:03 +0800

    增加第二個需求的測試案例2

--- PotterShoppingCart.Tests/ShoppingCartTest.cs
+++ PotterShoppingCart.Tests/ShoppingCartTest.cs
@@ -53,5 +53,28 @@ namespace PotterShoppingCart.Tests
             //  assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void CostCalcaulte_買了兩本相同的書_則維持原價()
+        {
+            //  arrange
+            var books = new List<Book>()
+            {
+                new Book{Name="哈利波特1", ISDN="00001", Amount=2},
+                new Book{Name="哈利波特2", ISDN="00002", Amount=0},
+                new Book{Name="哈利波特3", ISDN="00003", Amount=0},
+                new Book{Name="哈利波特4", ISDN="00004", Amount=0},
+                new Book{Name="哈利波特5", ISDN="00005", Amount=0},
+            };
+
+            int expected = 200;
+            var cashier = new CartCashier();
+
+            //  ack
+            int actual = cashier.GetExpense(books);
+
+            //  assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }

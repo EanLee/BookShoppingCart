5b7d407d4b8d6ce2cac7f45f0ea706750d2e1d9e
Ean Lee <ean@ebmtech.com>
2017-08-29 16:28:49 +0800

    增加第一個需求的測試案例

---
+++ PotterShoppingCart.Tests/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace PotterShoppingCart.Tests
+{
+    internal class Book
+    {
+        public string Name { get; set; }
+        public string ISDN { get; set; }
+        public int Amount { get; set; }
+    }
+}
---
+++ PotterShoppingCart.Tests/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PotterShoppingCart.Tests
+{
+    [TestClass]
+    public class ShoppingCart
+    {
+        [TestMethod]
+        public void CostCalcaulte_每本一百_如果只有買一本_應支付一百元()
+        {
+            //  arrange
+            var books = new List<Book>()
+            {
+                new Book{Name="哈利波特1", ISDN="00001", Amount=1},
+                new Book{Name="哈利波特2", ISDN="00002", Amount=0},
+                new Book{Name="哈利波特3", ISDN="00003", Amount=0},
+                new Book{Name="哈利波特4", ISDN="00004", Amount=0},
+                new Book{Name="哈利波特5", ISDN="00005", Amount=0},
+            };
+
+            int expected = 100;
+
+            //  ack
+            int actual = books.GetExpense();
+
+            //  assert
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}

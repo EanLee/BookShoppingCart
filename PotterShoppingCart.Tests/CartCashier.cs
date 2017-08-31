e3136bce63e59218fb5b34c7a5963f71475987c1
Ean Lee <ean@ebmtech.com>
2017-08-31 09:36:42 +0800

    第一次重構

--- PotterShoppingCart.Tests/CartCashier.cs
+++ PotterShoppingCart.Tests/CartCashier.cs
@@ -6,6 +6,20 @@ namespace PotterShoppingCart.Tests
 {
     public class CartCashier
     {
+        private Dictionary<int, double> _discountRule;
+
+        public CartCashier()
+        {
+            _discountRule = new Dictionary<int, double>
+            {
+                { 1, 1.00 },
+                { 2, 0.95 },
+                { 3, 0.90 },
+                { 4, 0.80 },
+                { 5, 0.75 }
+            };
+        }
+
         public int GetExpense(List<Book> books)
         {
             int maxAmount = books.Max(x => x.Amount);
@@ -17,27 +31,27 @@ namespace PotterShoppingCart.Tests
                 bookGroup.Add(amount);
             }
 
-            int total = 0;
+            double total = 0;
             foreach (var count in bookGroup)
             {
-                var cost = count * 100;
+                var cost = _discountRule[count] * 100 * count;
 
-                if (count == 2)
-                    cost = Convert.ToInt32(cost * 0.95);
+                //if (count == 2)
+                //    cost = Convert.ToInt32(cost * 0.95);
 
-                if (count == 3)
-                    cost = Convert.ToInt32(cost * 0.90);
+                //if (count == 3)
+                //    cost = Convert.ToInt32(cost * 0.90);
 
-                if (count == 4)
-                    cost = Convert.ToInt32(cost * 0.80);
+                //if (count == 4)
+                //    cost = Convert.ToInt32(cost * 0.80);
 
-                if (count == 5)
-                    cost = Convert.ToInt32(cost * 0.75);
+                //if (count == 5)
+                //    cost = Convert.ToInt32(cost * 0.75);
 
                 total += cost;
             }
 
-            return total;
+            return Convert.ToInt32(total);
         }
     }
 }
